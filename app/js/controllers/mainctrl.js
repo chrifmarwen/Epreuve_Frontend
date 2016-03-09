13c6a84c0695f44a40669f0895626d128d3be1d1
Chrif Marwen <marouen.chrif@gmail.com>
2016-03-09 21:35:18 +0100

    - fix

--- app/js/controllers/mainctrl.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = function ($scope, $rootScope, $state) {
-    $scope.currentUser = $rootScope.globals.currentUser;
-    $scope.uiRouterState = $state;
-    $scope.toggled = true;
-    $scope.toggleMenu = function() {
-        $scope.toggled = !$scope.toggled;
-    };
-};
